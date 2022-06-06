ae1178fc190f147b2ef3bf1cf305ef0db348ca85
Farhad Gharaie <f.gharaie@gmail.com>
2022-06-06 14:05:10 +0430

    ConvertCryptoToTraditional method test pass

--- ExchangeCurrency/ExchangeCurrency.UnitTest/ConvertCryptoToCurrencyTests.cs
+++ ExchangeCurrency/ExchangeCurrency.UnitTest/ConvertCryptoToCurrencyTests.cs
@@ -1,14 +1,34 @@
+using Exchange.Service;
+using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ExchangeCurrency.UnitTest
 {
-    public class UnitTest1
+    public class ConvertCryptoToCurrencyTests
     {
         [Fact]
-        public void Test1()
+        public void ConvertCryptocurrencyToTraditionalCurrency_WithSpecificCrypto_ReturenListOfCurrencies()
         {
+            //Arrange
+            var cryptoCurrency = "BTC";
+            var expectedConverts = new Dictionary<string, double>()
+            {
+                { "USD",1.1},
+                { "EUR",1.2},
+                { "BEL",1.3},
+                { "GBP",1.4},
+                { "AUD",1.5}
+            };
+
+            //Act
+            var actual = new ConvertService().ConvertCryptoToTraditional(cryptoCurrency);
+
+            //Assert
+            actual.Should().BeEquivalentTo(expectedConverts);
 
         }
     }
 }
+
