f00c3b77f73f61bf69dff53b598f51331f490ba4
Farhad Gharaie <f.gharaie@gmail.com>
2022-06-06 13:28:16 +0430

    initiate ExchangeRate unit test for convert crpto coin to currencies

---
+++ ExchangeRate.UnitTest/ExchangeRate.UnitTest/ConvertCryptoToCurrencyTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace ExchangeRate.UnitTest
+{
+    public class ConvertCryptoToCurrencyTests
+    {
+        [Fact]
+        public void ConvertCryptoToCurrency_WithSpecificCrypto_ReturenListOfCurrencies()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
