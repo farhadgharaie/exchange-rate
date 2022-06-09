1d3ecd2a604e25a195475c7c42c5f73b50ee3bf4
Farhad Gharaie <f.gharaie@gmail.com>
2022-06-09 17:52:53 +0430

    exchange crypto to traditional specflow test as BDD test created

---
+++ ExchangeCurrency/ExchangeCurrency.Specs/ExchnageCrytoToTraditionalSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ExchangeCurrency.Specs
+{
+    [Binding]
+    public class ExchnageCrytoToTraditionalSteps
+    {
+        [Given(@"the user entered BTC cryptocurrency symbol")]
+        public void GivenTheUserEnteredBTCCryptocurrencySymbol()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user press submit")]
+        public void WhenTheUserPressSubmit()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) in screen")]
+        public void ThenTheResultShouldBeInScreen(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
